72749cfd18bcc340a2d77b7d240f17be14f9aafc
Minho Eom <djaalsgh159@kaist.ac.kr>
2022-02-03 03:27:31 +0900

    calcium imaging

--- SimulMotif/calcium_imaging.py
+++ SimulMotif/calcium_imaging.py
@@ -2,7 +2,10 @@ import numpy as np
 from numpy.random import default_rng
 from tqdm import tqdm
 
-def create_calcium(spike_time, params, seed=None):
+from SimulMotif.motif_gen import *
+from SimulMotif.utils.draw_util import create_NLS_neuron_2d, generate_centers
+
+def create_calcium(spike_time, params, seed=0):
     """
     Generate calcium signal based on spike time and params.
     
@@ -12,7 +15,7 @@ def create_calcium(spike_time, params, seed=None):
         seed: random seed
     
     Returns:
-        calcium_signal
+        calcium_signal: calcium signal created (numpy ndarray with shape [NID, bins])
     """
     # check arguments
     risetime = int(params["physiological"]["risetime"])   # milliseconds
@@ -68,29 +71,97 @@ def create_calcium(spike_time, params, seed=None):
         
         # sampling
         convolved_signal = convolved_signal[sample_arr]
-        
+
         calcium_signal.append(convolved_signal)
-        
-    return np.array(calcium_signal)
+    
+    calcium_signal = np.array(calcium_signal, dtype=np.float32)
+
+    return calcium_signal
 
 
-def draw_calcium_image(calcium_signal, params, dimension=2, type="NLS", seed=None):
+def draw_calcium_image(params, seed=0):
     """
     Generate calcium imaging data based on calcium signal
     
     Arguments:
-        calcium_signal: calcium signal trace from "create_calcium" function.
-        dimension: whether the output is 2/3-dimensional
-        type: one of ["NLS"]
-            "NLS": nuclear-localized
-        seed: random seed
+        calcium_signal: calcium signal trace from "create_calcium" function. 
+        params: dictionary of configuration
+        seed: random seed 
     
     Returns:
-        calcium_image: [T, d1, d2, [d3]]
+        calcium_image: (numpy ndarray with size [T, d1, d2, [d3]])
+        ground_truth: dictionary including ground truth
     """
-    #check arguments
-    if not dimension in [2, 3]:
-        raise Exception("dimension must be 2 or 3")
+    # check arguments
+    if not params["shape"]["type"] in ["NLS"]:
+        raise Exception("type of shape is not supported")
+    fov = params["recording"]["FOV"]
+    if len(fov) not in [2]:
+        raise Exception("FOV must be 2-dimensional")
 
-    # draw image
-    
+    pixel_size = params["recording"]["pixel_size"]
+    diameter_nucleus = params["shape"]["diameter_nucleus"] / pixel_size         # in pixels
+    diameter_nucleus_std = params["shape"]["diameter_nucleus_std"] / pixel_size # in pixels
+    distance = np.array(params["shape"]["distance"]) / pixel_size               # in pixels
+    distance = distance.tolist()
+
+    # generate centers
+    centers = generate_centers(fov, min_dist=distance[0],\
+        max_dist=distance[1], seed=seed)
+
+    # generate calcium signal
+    params["NIDs"] = centers.shape[0]
+    spike_time, spike_time_motif = non_motif_gen(params, seed=seed)
+    # (Type 1) Precise synchronous spikes
+    gt1 = motif_gen(spike_time, spike_time_motif, 1, params, seed=1)
+    # (Type 2) Precise sequential spikes
+    gt2 = motif_gen(spike_time, spike_time_motif, 2, params, seed=2)
+    # (Type 3) Precise temporal pattern
+    gt3 = motif_gen(spike_time, spike_time_motif, 3, params, seed=3)
+    # (Type 4) Rate-based synchronous pattern
+    gt4 = motif_gen(spike_time, spike_time_motif, 4, params, seed=4)
+    # (Type 5) Rate-based sequential pattern
+    gt5 = motif_gen(spike_time, spike_time_motif, 5, params, seed=5)
+    calcium_signal = create_calcium(spike_time, params, seed=seed+1)
+
+    ground_truth = {
+        "spike_time": spike_time,
+        "spike_time_motif": spike_time_motif,
+        "gt1": gt1,
+        "gt2": gt2,
+        "gt3": gt3,
+        "gt4": gt4,
+        "gt5": gt5,
+        "calcium_signal": calcium_signal,
+        "centers": centers
+    }
+
+    # generate calcium image
+    calcium_image = np.zeros([calcium_signal.shape[1], *fov], dtype=np.float32)
+    for idx, center in enumerate(tqdm(centers, desc="generating calicum image")):
+        neuron = create_NLS_neuron_2d([int(diameter_nucleus/2), int(diameter_nucleus/2)], angle=0)
+        calcium_image_neuron = np.outer(calcium_signal[idx], neuron)
+        calcium_image_neuron = np.reshape(calcium_image_neuron, [-1] + list(neuron.shape))
+
+        # shifts
+        shifts = np.array(neuron.shape)/2
+        shifts = shifts.astype(np.int32)
+        calcium_image_start = center - shifts
+        calcium_image_end = calcium_image_start + np.array(neuron.shape, dtype=np.int32)
+        offset_start = np.maximum(calcium_image_start, 0) - calcium_image_start
+        offset_end = calcium_image_end - np.minimum(calcium_image_end, fov)
+        in_fov = np.array(neuron.shape, dtype=np.int32) > offset_start + offset_end # neuron inside fov
+        if False in in_fov:
+            continue
+        
+        # add signal
+        calcium_image[:, calcium_image_start[0]+offset_start[0]:calcium_image_end[0]-offset_end[0],\
+            calcium_image_start[1]+offset_start[1]:calcium_image_end[1]-offset_end[1]] +=\
+             calcium_image_neuron[:, offset_start[0]:neuron.shape[0]-offset_end[0],\
+             offset_start[1]:neuron.shape[1]-offset_end[1]]
+
+    return calcium_image, ground_truth
+
+
+if __name__=="__main__":
+    pass
